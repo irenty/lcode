c5cfd0b45cba76e1fef77052777bda7b2fd160df
ireneusz-marciniak <ireneusz.marciniak@schibsted.com>
2018-11-21 23:24:19 +0000

    0005 testing support

--- src/main/scala/_0005_longest_palindromic_substring/Solution.scala
+++ src/main/scala/_0005_longest_palindromic_substring/Solution.scala
@@ -1,15 +1,10 @@
 package _0005_longest_palindromic_substring
-
 object Solution {
-  def longestPalindrome(s: String): String = {
-
+  class SolutionHelper(s: String) {
     def findPalindrom(indexStart: Int, indexEnd: Int, letters: Int): String =
       if (indexStart - letters < 0 || indexEnd + letters >= s.size) s.slice(indexStart - letters + 1, indexEnd + letters)
       else if (s(indexStart - letters) != s(indexEnd + letters)) s.slice(indexStart - letters + 1, indexEnd + letters)
       else findPalindrom(indexStart, indexEnd, letters + 1)
-
-    if (s.isEmpty) return ""
-    if (s.size == 1) return s
 
     def findIndexEnd(indexStart: Int, indexEnd: Int): Int =
       if (indexEnd > s.length - 1) indexEnd - 1
@@ -23,11 +18,28 @@ object Solution {
       val newLongest = if (palindrom.size > longestFound.size) palindrom else longestFound
       findLongest(indexEnd + 1, newLongest)
     }
+  }
 
-    findLongest(0, s.take(1))
+  object SolutionHelper {
+    def apply(s: String): SolutionHelper = new SolutionHelper(s)
+  }
+
+  def solutionHelperTest(): String = {
+    // SolutionHelper("abcc").findIndexEnd(2, 2).toString
+    // SolutionHelper("tyuabcaacbaxyz").findPalindrom(6, 7, 1).toString
+    ""
+  }
+
+  def longestPalindrome(s: String): String = {
+    //solutionHelperTest()
+
+    if (s.isEmpty) return ""
+    if (s.size == 1) return s
+
+    SolutionHelper(s).findLongest(0, s.take(1))
+
   }
 }
-
 
 object Main0005 extends App {
   println(Solution.longestPalindrome("bb"))
